0243bd3528cb4ef6b494b4f4bbf62bd56a746433
hoff <micklinghoff@gmail.com>
2022-03-01 22:37:42 +0100

    initial setup

---
+++ main.py
@@ -0,0 +1,38 @@
+import datetime
+
+from flask import Flask, render_template
+
+from flask_sse import sse
+
+app = Flask(__name__)
+
+
+app.config["REDIS_URL"] = "redis://localhost"
+app.register_blueprint(sse, url_prefix='/stream')
+
+@app.route('/send')
+def send_message():
+    sse.publish({"message": "Hello!"}, type='greeting')
+    return "Message sent!"
+
+
+@app.route('/')
+def root():
+    # For the sake of example, use static information to inflate the template.
+    # This will be replaced with real information in later steps.
+    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
+                   datetime.datetime(2018, 1, 2, 10, 30, 0),
+                   datetime.datetime(2018, 1, 3, 11, 0, 0),
+                   ]
+
+    return render_template('index.html', times=dummy_times)
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
